bf35065c17e85f8c65454631a0e352a3316dc876
Josi Reis-West <josialexa85@gmail.com>
2019-12-03 11:38:07 -0600

    Completed step 5

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mc = require('./controllers/messages_controller.js');
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/api/messages', mc.read);
+app.post('/api/messages', mc.create);
+app.put('/api/messages/:id', mc.update);
+app.delete('/api/messages/:id', mc.delete);
 
 app.listen(3001, () => {
     console.log('Listening on port 3001');
